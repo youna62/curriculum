74a4ba853bf64d9c047bd37c1cc69ed53584e3a0
Subhin Hwang <107633586+Surihub@users.noreply.github.com>
2024-06-29 07:12:34 +0000

    fix

--- app.py
+++ app.py
@@ -13,14 +13,17 @@ level_options = curri['학교급'].dropna().unique().tolist()
 area_options = curri['영역'].dropna().unique().tolist()
 subject_options = curri['과목'].dropna().unique().tolist()
 
-selected_revisions = st.multiselect("개정 일시 선택", revision_options)
-selected_levels = st.multiselect("학교급 선택", level_options)
-selected_areas = st.multiselect("영역 선택", area_options)
-selected_subjects = st.multiselect("과목 선택", subject_options)
+col1, col2 = st.columns(2)
+with col1 :    
+    selected_revisions = st.multiselect("개정 일시 선택", revision_options)
+    selected_levels = st.multiselect("학교급 선택", level_options)
+with col2 :
+    selected_areas = st.multiselect("영역 선택", area_options)
+    selected_subjects = st.multiselect("과목 선택", subject_options)
 
 
 # 키워드 검색 필드 강조
-st.markdown("## **키워드 검색**")
+st.markdown("## **성취기준 키워드 검색**")
 keyword = st.text_input("키워드 검색", placeholder="검색할 키워드를 입력하세요...", key="keyword_input", max_chars=50)
 
 # 필터 적용
@@ -37,7 +40,7 @@ if keyword:
     filtered_curri = filtered_curri[filtered_curri.apply(lambda row: row.astype(str).str.contains(keyword).any(), axis=1)]
 
 # 데이터프레임 출력
-st.dataframe(filtered_curri)
+st.dataframe(filtered_curri[['영역', '성취기준']])
 
 # 메시지 출력
 if filtered_curri.empty:
